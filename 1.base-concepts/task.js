9ea1b7bfc8964d18901984b9933dd81417836841
astsidvan <107125568+astsidvan@users.noreply.github.com>
2023-01-17 16:07:21 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = b**2-4*a*c;
+  if (discriminant < 0) {
+    arr = []; 
+  }
+    else if (discriminant = 0) {
+      arr = [-b/(2*a)];
+    }
+    else if (discriminant > 0) {
+      arr = [(-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a)];
+    }
   
   return arr;
 }
