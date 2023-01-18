a1d03fe211e665072d5d14f4b3afd3f4c281bb92
astsidvan <astsidvan@gmail.com>
2023-01-18 12:13:03 +0300

    Правки по первому заданию

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,14 +3,12 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b**2-4*a*c;
-  if (discriminant < 0) {
-    arr = []; 
-  }
-    else if (discriminant = 0) {
+ 
+    if (discriminant == 0) {
       arr = [-b/(2*a)];
     }
     else if (discriminant > 0) {
-      arr = [(-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a)];
+      arr = [(-b + Math.sqrt(d) )/(2*a) , (-b - Math.sqrt(d) )/(2*a)];
     }
   
   return arr;
