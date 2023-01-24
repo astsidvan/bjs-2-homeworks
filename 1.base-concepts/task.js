9dc4d15d99fdace312f7dbe8f6a23a2757664381
astsidvan <astsidvan@gmail.com>
2023-01-24 12:31:39 +0300

    Правки по первому заданию

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,39 +8,21 @@ function solveEquation(a, b, c) {
       arr = [-b/(2*a)];
     }
     else if (discriminant > 0) {
-      arr = [(-b + Math.sqrt(d) )/(2*a) , (-b - Math.sqrt(d) )/(2*a)];
+      arr = [(-b + Math.sqrt(discriminant) )/(2*a) , (-b - Math.sqrt(discriminant) )/(2*a)];
     }
   
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  if (isNaN(percent)) {
+  if (isNaN(percent || contribution || amount || date)) {
     return false;
   }
-  if (isNaN(contribution)) {
-    return false;
-  }
-  if (isNaN(amount)) {
-    return false;
-  }
-  if (isNaN(date)) {
-    return false;
-  }
-  let bodyCredit = 0;
-
+  
   let newPesent = percent / 100 / 12;
-  if (contribution) {
-    bodyCredit = amount - contribution;
-  } else {
-    bodyCredit = amount;
-  }
-
+  let bodyCredit = amount - contribution;
   let monthlyPayment =
-    bodyCredit * (newPesent + newPesent / ((1 + newPesent)  date - 1));
+    bodyCredit * (newPesent + newPesent / ((1 + newPesent) ** date - 1));
   let totalAmount = Number((monthlyPayment * date).toFixed(2));
 
   return totalAmount;
